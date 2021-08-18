06f98afcab598a76f7551dcb2493547b4fd2e471
Ayush Pradhan <ayupradhan1999@gmail.com>
2021-08-18 17:46:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -35,9 +35,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Ayush Pradhan
           </p>
-          <LambdaDemo />
         </header>
       </div>
     )
